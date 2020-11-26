aec86089ea84e3fe0f6b7acd283ce02344c2e279
Aleksandr Kukhtin <alex.kukhtin.dev@gmail.com>
2020-11-26 22:57:38 +0200

    fixed some unused properties in gitignore

---
+++ src/main/java/com/akukhtin/deviceinformationapplication/DeviceInformationApplication.java
@@ -0,0 +1,13 @@
+package com.akukhtin.deviceinformationapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceInformationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DeviceInformationApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/akukhtin/deviceinformationapplication/DeviceInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akukhtin.deviceinformationapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceInformationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
