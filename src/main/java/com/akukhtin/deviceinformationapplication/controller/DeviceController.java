adcba30183ac6d45f0d2e2eadc45febf621ddbbb
Aleksandr Kukhtin <alex.kukhtin.dev@gmail.com>
2020-11-27 12:54:56 +0200

    changed some logic in controllers

--- src/main/java/com/akukhtin/deviceinformationapplication/controller/DeviceController.java
+++ src/main/java/com/akukhtin/deviceinformationapplication/controller/DeviceController.java
@@ -3,8 +3,6 @@ package com.akukhtin.deviceinformationapplication.controller;
 import com.akukhtin.deviceinformationapplication.dto.DeviceDto;
 import com.akukhtin.deviceinformationapplication.entity.Device;
 import com.akukhtin.deviceinformationapplication.services.DeviceService;
-import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,12 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/device/")
 public class DeviceController {
-  @Autowired
-  private DeviceService deviceService;
+  private final DeviceService deviceService;
+
+  public DeviceController(DeviceService deviceService) {
+    this.deviceService = deviceService;
+  }
 
   @PostMapping("devices")
-  public ResponseEntity<Optional<Device>> create(@RequestBody DeviceDto deviceDto) {
-    Optional<Device> device = deviceService.create(deviceDto);
-    return ResponseEntity.ok(device);
+  public ResponseEntity<Device> create(@RequestBody DeviceDto deviceDto) {
+    return ResponseEntity.ok(deviceService.create(deviceDto));
   }
 }
--- src/main/java/com/akukhtin/deviceinformationapplication/controller/DeviceWithAlarmsController.java
+++ src/main/java/com/akukhtin/deviceinformationapplication/controller/DeviceWithAlarmsController.java
@@ -1,7 +1,27 @@
 package com.akukhtin.deviceinformationapplication.controller;
 
+import com.akukhtin.deviceinformationapplication.dto.DeviceWithAlarmsDto;
+import com.akukhtin.deviceinformationapplication.entity.DeviceWithAlarms;
+import com.akukhtin.deviceinformationapplication.services.DeviceWithAlarmsService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/device-with-alarms/")
 public class DeviceWithAlarmsController {
+
+  private final DeviceWithAlarmsService deviceWithAlarmsService;
+
+  public DeviceWithAlarmsController(DeviceWithAlarmsService deviceWithAlarmsService) {
+    this.deviceWithAlarmsService = deviceWithAlarmsService;
+  }
+
+  @PostMapping("devices-with-alarms")
+  public ResponseEntity<DeviceWithAlarms> create(
+          @RequestBody DeviceWithAlarmsDto deviceWithAlarmsDto) {
+    return ResponseEntity.ok(deviceWithAlarmsService.create(deviceWithAlarmsDto));
+  }
 }
